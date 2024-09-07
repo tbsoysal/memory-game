5e52591a06b1c3d9dde402229d402ea935cfc57a
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-09-07 18:45:23 +0300

    recreate the app logic and add comments

--- js/card.js
+++ js/card.js
@@ -1,9 +1,31 @@
 export default class Card {
-  constructor(value) {
-    this.value = value;
+  constructor(emoji) {
+    this.emoji = emoji;
     this.flipped = false;
-    this.matched = false;
+    this.element = document.createElement('div');
+    this.createElement();
   }
-}
 
+  createElement() {
+    this.element.classList.add('card');
 
+    this.element.innerHTML = `
+      <div class="card-inner">
+        <div class="card-front"></div>
+        <div class="card-back">
+          <img src="${this.emoji}" alt="emoji">
+        </div>
+      </div>
+`;
+  }
+
+  showEmoji() {
+    this.flipped = true;
+    this.element.classList.add('open');
+  }
+
+  hideEmoji() {
+    this.flipped = false;
+    this.element.classList.remove('open');
+  }
+};
--- js/game.js
+++ js/game.js
@@ -1,73 +1,98 @@
 import Card from './card.js';
 
 export default class Game {
-  constructor(size) {
-    this.size = size;
+  constructor() {
     this.cards = [];
     this.flippedCards = [];
     this.matchedPairs = 0;
+    this.cardEmojies = [
+      './assets/arı.png',
+      './assets/civciv.png',
+      './assets/ejderha.png',
+      './assets/flamingo.png',
+      './assets/hamster.png',
+      './assets/köpek.png',
+      './assets/unicorn.png',
+      './assets/yonca.png'
+    ];
     this.generateCards();
   }
 
   generateCards() {
-    const values = ['./assets/arı.png',
-                    './assets/yonca.png',
-                    './assets/civciv.png',
-                    './assets/köpek.png',
-                    './assets/ejderha.png',
-                    './assets/hamster.png',
-                    './assets/unicorn.png',
-                    './assets/flamingo.png'];
-    const cardValues = [...values, ...values];
-    this.shuffle(cardValues);
+    // Duplicate emojies to have 2 instance of each emoji
+    this.cardEmojies = [...this.cardEmojies, ...this.cardEmojies];
+    // Shuffle the emoji addresses
+    this.shuffleEmojies();
+    // For each emojiAddress create a card object and add the cards array
+    for (const emojiAddress of this.cardEmojies) {
+      const card = new Card(emojiAddress);
+      this.cards.push(card);
+    }
 
-    for (let value of cardValues) {
-      this.cards.push(new Card(value));
+    // Take the game board element from dom and clear its inner html
+    const board = document.getElementById('game-board');
+    board.innerHTML = '';
+
+    // for each card in the cards array add the card.element (html element) to the board and listen for click
+    for (const card of this.cards) {
+      board.append(card.element);
+      card.element.addEventListener('click', ()=>this.flipCard(card));
     }
   }
 
-  shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+  shuffleEmojies() {
+    for(let i = this.cardEmojies.length - 1; i > 0; i--) {
+      // Generate a random index from 0 to i
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+
+      // Swap elements array[i] and array[j];
+      [this.cardEmojies[i], this.cardEmojies[j]] = [this.cardEmojies[j], this.cardEmojies[i]];
     }
   }
 
-  flipCard(cardElement, index) {
-    if (this.flippedCards.length === 2 || this.cards[index].flipped) return;
+  async flipCard(card) {
+    // check if the card is already flipped
+    if (card.flipped || this.flippedCards.length === 2)
+      return;
 
-    this.cards[index].flipped = true;
-    this.flippedCards.push({cardElement, index});
-    cardElement.classList.add('open');
-    if (this.flippedCards.length === 2) {
-      this.checkMatch();
+    // if not call showEmoji function of the card that makes flipped porperty true and add 'open' class name to the card
+    card.showEmoji();
+    // Add the card to the flippedCards array
+    this.flippedCards.push(card);
+
+    // If flippedCards has a 2 card inside check for matching, then reset the flippedCards array
+    if (this.flippedCards.length == 2){
+      await this.checkMatch();
+      this.flippedCards = [];
     }
   }
 
   checkMatch() {
+    // Get the first card and the second card from the flippedCards array
     const [firstCard, secondCard] = this.flippedCards;
-    const first = this.cards[firstCard.index];
-    const second = this.cards[secondCard.index];
-
-    if (first.value === second.value) {
-      first.matched = true;
-      second.matched = true;
-      this.matchedPairs++;
-    } else {
-      setTimeout(() => {
-        first.flipped = false;
-        second.flipped = false;
-        firstCard.cardElement.classList.remove('open');
-        secondCard.cardElement.classList.remove('open');
-      }, 800);
+    // If they have a same emoji address check for if the game is over
+    if (firstCard.emoji === secondCard.emoji) {
+      this.isGameOver();
     }
-    setTimeout(() => {
-      this.flippedCards = [];
-    }, 900);
+    else {
+      // If they not wait 800ms to show cards to the player, then flip back firstCard and secondCard 
+      return new Promise(res => setTimeout(() => {
+        firstCard.hideEmoji();
+        secondCard.hideEmoji();
+        res();
+      }, 800));
+    }
   }
 
   isGameOver() {
-    return this.matchedPairs === this.size / 2;
+    // Increase one the matchedPairs variable
+    this.matchedPairs++;
+    if(this.matchedPairs === 8) {
+      setTimeout(() => {
+        if(window.confirm('Congralations! You won! Do you want to restart the game?')){
+          const newGame = new Game();
+        }
+      }, 1000);
+    }
   }
-
 }
